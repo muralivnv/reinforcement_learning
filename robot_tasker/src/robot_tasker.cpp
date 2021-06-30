9d9f0fba342e4fd39464734b03a0cd4a337f2535
muralivnv <muralivnv@gmail.com>
2021-06-30 11:28:22 -0400

    replaced abs global params path with rel path

--- robot_tasker/src/robot_tasker.cpp
+++ robot_tasker/src/robot_tasker.cpp
@@ -2,19 +2,21 @@
 #include "learning/to_drive/to_drive.h"
 #include "learning/to_drive/to_drive_visualization.h"
 
+#include "util/util.h"
 #include "util/environment_util.h"
 
 using namespace ANN;
 using namespace learning::to_drive;
 
-#define CONFIG_FILE ("X:/Video_Lectures/ReinforcementLearning/scripts/robot_tasker/global_params.yaml")
+#define CONFIG_FILE ("../global_params.yaml")
 
 int main()
 {
-  auto global_config  = env_util::read_global_config(CONFIG_FILE);
+  auto config_full_path = util::get_file_dir_path(__FILE__) + "/" + CONFIG_FILE;
+  auto global_config = env_util::read_global_config(config_full_path);
 
   auto [actor_network, critic_network] = learn_to_drive(global_config);
-  show_me_what_you_learned(actor_network, critic_network, CONFIG_FILE, 2u);
+  show_me_what_you_learned(actor_network, critic_network, config_full_path, 2u);
 
   return EXIT_SUCCESS;
 }
